91e401d7133224ef4d3c02ddeea9bd4ab111ab45
DutraGames <gabrieldutraamarante@gmail.com>
2023-07-16 12:24:21 -0300

    configuration svg component

---
+++ svg-template.js
@@ -0,0 +1,20 @@
+const Template = (
+    { imports, interfaces, componentName, props, jsx, exports },
+    { tpl },
+  ) => {
+    return tpl`${imports}
+  ${interfaces}
+  interface IconProps {
+    size?: number | string;
+    color?: string;
+  }
+  
+  function ${componentName}({size = 32,color = "#000000",}:IconProps, ${props}) {
+    return ${jsx};
+  }
+  
+  ${exports}
+    `
+  }
+  
+  module.exports = Template
