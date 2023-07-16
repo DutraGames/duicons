991457df3bc4e9523a27c16beabddb9e850394ae
DutraGames <gabrieldutraamarante@gmail.com>
2023-07-16 12:23:16 -0300

    rollup configuration

---
+++ rollup.config.mjs
@@ -0,0 +1,34 @@
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
+import filesize from 'rollup-plugin-filesize';
+
+export default [
+  {
+    input: 'src/components/index.ts',
+    output: [
+      {
+        file: 'dist/cjs/index.js',
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: 'dist/esm/index.esm.js',
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      commonjs(),
+      typescript({ tsconfig: './tsconfig.json' }),
+      filesize(),
+    ],
+  },
+  {
+    input: 'dist/esm/types/components/index.d.ts',
+    output: [{ file: 'dist/types/index.d.ts', format: 'esm' }],
+    plugins: [
+      dts()
+    ],
+  },
+];
