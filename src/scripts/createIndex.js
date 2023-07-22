309becbc743a959e2f6fbd52dfb4bb05a28cdbed
DutraGames <gabrieldutraamarante@gmail.com>
2023-07-22 10:12:25 -0300

    --feat create functions for index.ts create

---
+++ src/scripts/createIndex.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+
+
+const createIndex = () => {
+
+    const content = 'export * as default from "./components";';
+    const filePath = path.join(__dirname, '../', 'index.ts');
+
+    fs.writeFile(filePath, content, (err) => {
+        if (err) return console.error('Error in creating index.ts: ', err);
+
+        console.log('Success in creating index.ts!');
+    }
+    );
+}
+
+createIndex();
