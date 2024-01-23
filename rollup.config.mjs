e73a63899df916f47021accbb6f94c04e50b536a
DutraGames <gabrieldutraamarante@gmail.com>
2024-01-23 11:58:32 -0300

    refactor: update and mofied

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,34 +1,32 @@
-import commonjs from '@rollup/plugin-commonjs';
-import typescript from '@rollup/plugin-typescript';
-import dts from 'rollup-plugin-dts';
-import filesize from 'rollup-plugin-filesize';
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+import filesize from "rollup-plugin-filesize";
 
 export default [
   {
-    input: 'src/index.ts',
+    input: "src/index.ts",
     output: [
       {
-        file: 'dist/cjs/index.js',
-        format: 'cjs',
+        file: "dist/cjs/index.js",
+        format: "cjs",
         sourcemap: true,
       },
       {
-        file: 'dist/esm/index.esm.js',
-        format: 'esm',
+        file: "dist/esm/index.esm.js",
+        format: "esm",
         sourcemap: true,
       },
     ],
     plugins: [
       commonjs(),
-      typescript({ tsconfig: './tsconfig.json' }),
+      typescript({ tsconfig: "./tsconfig.json" }),
       filesize(),
     ],
   },
   {
-    input: 'dist/esm/types/index.d.ts',
-    output: [{ file: 'dist/types/index.d.ts', format: 'esm' }],
-    plugins: [
-      dts()
-    ],
+    input: "dist/esm/types/index.d.ts",
+    output: [{ file: "dist/types/index.d.ts", format: "esm" }],
+    plugins: [dts()],
   },
 ];
